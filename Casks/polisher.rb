4984b8765c93aef82819b4c5c828858c20c5694c
chezihoyzer <choyzer@triplewhale.com>
2026-02-24 20:28:02 +0200

    Bump Polisher to v2.8

--- Casks/polisher.rb
+++ Casks/polisher.rb
@@ -1,6 +1,6 @@
 cask "polisher" do
-  version "2.7"
-  sha256 "395751014dbdb1c852c10243bf54d96429775d27a63226b665fbdf7e88d53fb9"
+  version "2.8"
+  sha256 "5ecb7125bbd80a623269d6dfa0b5f4dfe9f8ecc91544d2623243f4b864951ca0"
 
   url "https://github.com/Triple-Whale/Polisher/releases/download/v#{version}/Polisher.dmg"
   name "Polisher"
